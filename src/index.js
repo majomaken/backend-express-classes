2708222105dcc5393d9d55b9e93021c01aa8eca5
majomaken <majo.maken@gmail.com>
2024-08-09 09:12:17 -0500

    add sequlize and sql class

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 const { Pool } = require("pg");
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+  "postgres://postgres:HfwTHbIwj1b4D7YkMK@localhost:5432/my-first-db"
+)
+
+const connectDb = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+connectDb();
 
 const pool = new Pool({
   user: "postgres",
@@ -20,29 +35,40 @@ pool.query("SELECT NOW()", (error, response) => {
   }
 });
 
-let tasks = [
-  { id: 1, description: "Aprender Node.js", completed: false },
-  { id: 2, description: "Practicar Express", completed: false },
-];
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get("/tasks", (request, response) => {
-  response.json({ tasks });
+app.get("/tasks", async (request, response) => {
+  const results = await pool.query(
+    "SELECT tasks.*, users.name FROM tasks JOIN users ON tasks.user_id = users.id ORDER BY tasks.id ASC"
+  )
+  response.json({ tasks: results.rows });
 });
+
+app.get("/users", async (request, response) => {
+  try {
+    const queryUsers = await pool.query(
+      "SELECT * FROM users"
+    )
+    response.json(queryUsers.rows)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.post("/tasks", async (request, response) => {
   try {
     const newTaskBody = {
       description: request.body.description,
       status: request.body.status || false,
+      userId: request.body.userId || 1,
     };
 
     const result = await pool.query(
-      "INSERT INTO tasks (description, status) VALUES ($1, $2) RETURNING *",
-      [newTaskBody.description, newTaskBody.status]
+      "INSERT INTO tasks (description, status, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [newTaskBody.description, newTaskBody.status, newTaskBody.userId]
     );
     response.status(201).json(result.rows[0]);
     console.log("Tarea creada correctamente");
@@ -52,23 +78,31 @@ app.post("/tasks", async (request, response) => {
   }
 });
 
-app.put("/tasks/:id", (request, response) => {
+app.put("/tasks/:id", async (request, response) => {
   const taskId = parseInt(request.params.id);
-  const taskIndex = tasks.findIndex((task) => task.id === taskId);
-  if (taskIndex === -1) {
-    return response.status(404).json({ error: "Tarea no encontrada" });
-  }
-  if (request.body.description)
-    tasks[taskIndex].description = request.body.description;
-  if (request.body.completed)
-    tasks[taskIndex].completed = request.body.completed;
-  response.status(200).json(tasks[taskIndex]);
+
+  // const updateBody = {
+  //   userId: request.body.userId,
+  // }
+
+  const results = await pool.query(
+    "UPDATE tasks SET user_id = $1 WHERE id = $2 RETURNING *",
+    [request.body.userId, taskId]
+  )
+
+  response.status(200).json(results.rows[0]);
 });
 
-app.delete("/tasks/:id", (request, response) => {
+app.delete("/tasks/:id", async (request, response) => {
   const taskId = parseInt(request.params.id);
-  tasks = tasks.filter((task) => task.id !== taskId);
-  response.status(200).json({ tasks, message: "Tarea eliminada" });
+
+  if (isNaN(taskId) || taskId === 0 || !taskId) {
+    return response.status(400).json({ error: "El ID de la tarea no es válido" });
+  }
+
+  await pool.query("DELETE FROM tasks WHERE id = $1", [taskId]);
+
+  response.status(200).json({ message: "Tarea eliminada" });
 });
 
 app.listen(PORT, () => {
